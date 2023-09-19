d473179ea815f8724601cc3db5be5bc514a70a6f
muhsinabdil <muhsinabdil@hotmail.com>
2023-09-19 21:34:07 +0300

    node server start project

---
+++ data.js
@@ -0,0 +1,15 @@
+module.exports=[
+{
+id:1,
+isim:"Kemal Sunal",
+filmler:["Sakar Şakir", "Tosun Paşa"],
+},{
+    id:2,
+    isim:"Şener Şen",
+    filmler:["Kabadayı", "Tosun Paşa"],
+    },{
+        id:3,
+        isim:"Adile Naşit",
+        filmler:["Hababam", "Tosun Paşa"],
+        }
+];
--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 
-const http=require('http');
-const express=require ("express");
-const app =express();
+const express = require("express");
+const server = express();
 
-const host='127.0.0.1'
-const port =3000;
-const server =http.createServer((req,res)=>{
-cevap.statusCode=200;
-cevap.setHeader('Content-Type','text/plain');
-cevap.end('React Dersleri Başlıyor');
+const host = '127.0.0.1'
+const port = 3333;
+const data = require('./data.js');
+
+
+server.get("/", function (req, res) {
+
+    res.send('Merhaba Dünya');
 
 });
 
-app.use("/",function( req,res){
 
+server.get("/aktorler", function (req, res) {
 
- 
+    res.status(200).json(data);
+
 });
 
+server.get("/aktorler/:id", function (req, res) {
 
-app.listen(port,function(){
+  const {id} =req.params;
+const aktor=data.find(x=>x.id===parseInt(id))
 
-  
+if(aktor){
+  res.status(200).json(aktor);
+}else{
+    res.status(404).send("Aradığınız Sayfa Bulunamadı")
+}
+});
+
+server.listen(port, host, function () {
+
+    console.log(`http://${host} adresi dinleniyor`);
+
 });
