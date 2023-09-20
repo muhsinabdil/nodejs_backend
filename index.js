1b22a2e7712aa5d25a1fdbc406f9ce412c4941b0
muhsinabdil <muhsinabdil@hotmail.com>
2023-09-20 20:49:04 +0300

    nodemon

--- index.js
+++ index.js
@@ -4,36 +4,21 @@ const server = express();
 
 const host = '127.0.0.1'
 const port = 3333;
-const data = require('./data.js');
 
+
+const aktorlerRouter=require("./routers/aktorlerRouter")
 
 server.get("/", function (req, res) {
 
-    res.send('Merhaba Dünya');
+    res.send('Merhaba Router');
 
 });
 
-
-server.get("/aktorler", function (req, res) {
-
-    res.status(200).json(data);
-
-});
-
-server.get("/aktorler/:id", function (req, res) {
-
-  const {id} =req.params;
-const aktor=data.find(x=>x.id===parseInt(id))
-
-if(aktor){
-  res.status(200).json(aktor);
-}else{
-    res.status(404).send("Aradığınız Sayfa Bulunamadı")
-}
-});
+server.use("/aktorler", aktorlerRouter);
 
 server.listen(port, host, function () {
 
     console.log(`http://${host} adresi dinleniyor`);
 
 });
+
---
+++ routers/aktorlerRouter.js
@@ -0,0 +1,40 @@
+const router =require("express").Router();
+
+let data=require("../data.js");
+
+
+
+
+
+
+router.get("/", function (req, res) {
+
+    res.status(200).json(data);
+
+});
+
+router.get("/:id", function (req, res) {
+
+  const {id} =req.params;
+const aktor=data.find(x=>x.id===parseInt(id))
+
+if(aktor){
+  res.status(200).json(aktor);
+}else{
+    res.status(404).send("Aradığınız Sayfa Bulunamadı")
+}
+});
+
+
+let next_id=4;
+
+router.post("/",(req,res)=>{
+
+let yeniAktor=req.body;
+yeniAktor.id=next_id;
+next_id++;
+console.log("idimiz:",next_id);
+data.push(yeniAktor);
+res.status(201).json(yeniAktor);
+
+});
